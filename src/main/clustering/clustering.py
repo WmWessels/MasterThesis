34b83814edc974aaad7f6c09f6dd00f7327c1b3d
Wman1001 <wichert.wessels@live.nl>
2023-02-28 11:46:02 +0100

    added subclasses for clustering algorithms

--- src/main/clustering/clustering.py
+++ src/main/clustering/clustering.py
@@ -1,18 +1,66 @@
 import numpy as np
 import pandas as pd
 
+from typing import Protocol
+
 from pandas import DataFrame
-from sklearn.cluster import KMeans
+from kernel_kmeans import KernelKMeans
 
-class MetaDataCluster:
+from sklearn.cluster import OPTICS
 
-    def __init__(self, dataset: DataFrame):
-        self.dataset = dataset
+class MetaCluster(Protocol):
 
+    def load_from_file(self):
+        ...
 
+    def save_to_file(self):
+        ...
+    
+    def pred_to_portfolio(self):
+        ...
+
+class MetaKernelKMeans(KernelKMeans):
+    """ Small wrapper around KernelKMeans to make it more usable for our purposes """
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        
+    def load_from_file(self):
+        """
+        Future function to load clustering model from a file
+        """
+        pass
 
     def save_to_file(self):
         """
-        Future function to save clustering model to a file as pmml
+        Future function to save clustering model to a file
+        """
+        pass
+
+    def pred_to_portfolio(self):
+        """
+        Future function to predict the portfolio based on the clustering model
+        """
+        pass
+
+class MetaOPTICS(OPTICS):
+    """ Small wrapper around OPTICS to make it more usable for our purposes """
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        
+    def load_from_file(self):
+        """
+        Future function to load clustering model from a file
+        """
+        pass
+
+    def save_to_file(self):
+        """
+        Future function to save clustering model to a file
+        """
+        pass
+
+    def pred_to_portfolio(self):
+        """
+        Future function to predict the portfolio based on the clustering model
         """
         pass
